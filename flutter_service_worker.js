99678e0b610c8dbecd6620dcfb433b89d68134ed
DeathVR <64973519+DeathVR@users.noreply.github.com>
2024-11-20 16:15:31 +0500

    platform

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "df616e6787f42b091e35e88678056238
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "fcfbd646264e3647b892796ab0d31484",
+"flutter_bootstrap.js": "74f83056ed6dbf3a5454229c33af0ecf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "efdb915ef431eb90c6960e2de54d5600",
 "/": "efdb915ef431eb90c6960e2de54d5600",
-"main.dart.js": "59fe232594eae85978204c39c1154c9c",
+"main.dart.js": "b06ee90d377c849b342324ead5742ba2",
 "manifest.json": "2c14330b75a814d2326be4f6811354c9",
 "version.json": "47773e16976ed264d1087c66b74fcd66"};
 // The application shell files that are downloaded before a service worker can
